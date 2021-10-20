dfa38f7a8b58166515dcdc603b05bd8ce4201d10
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-20 20:14:36 -0300

    chore: class-35

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ const path = require('path')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+mongoose.mongo.Promise = global.Promise
+mongoose.connect('mongodb://localhost/blogapp')
+    .then(() => {
+        console.log('DB connected')
+    })
+    .catch((err) => {
+        console.log('Error connectiong to database: ' + err)
+    })
 
 app.use(express.static(path.join(__dirname, 'public')))
 
