a14538e456c1c60253d133275d6ebd317414f1c0
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-20 19:11:16 -0300

    chore: class-32

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const mongoose = require('mongoose')
 const app = express()
+const admin = require('./routes/admin')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+app.use('/admin', admin)
 
 const port = 8080
 app.listen(port, () => {
