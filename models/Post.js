03fed8f7e4d2e72c0164963d47b83105a11553ec
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-20 23:23:57 -0300

    chore: class-44

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Postagem = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    slug: {
+        type: String,
+        require: true
+    },
+    desctiption: {
+        type: String,
+        require: true
+    },
+    contend: {
+        type: String,
+        require: true
+    },
+    category: {
+        type: Schema.Types.ObjectID,
+        ref: 'categories',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+mongoose.model('postagens', Postagem)
