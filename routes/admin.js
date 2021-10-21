5ed25125e155c055a05b9343492f3b2316385048
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-20 22:33:29 -0300

    chore: class-42

--- routes/admin.js
+++ routes/admin.js
@@ -13,12 +13,15 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categories', (req, res) => {
-    Category.find().sort({date: 'desc'}).then((categories) => {
-        res.render('admin/categories', {categories: categories})
-    }).catch((err) => {
-        req.flash('error_msg', 'Show categories got an errour, please try again')
-        res.redirect('admin')
-    })
+    Category.find()
+        .sort({ date: 'desc' })
+        .then((categories) => {
+            res.render('admin/categories', { categories: categories })
+        })
+        .catch((err) => {
+            req.flash('error_msg', 'Show categories got an errour, please try again')
+            res.redirect('admin')
+        })
 })
 
 router.get('/categories/add', (req, res) => {
@@ -59,6 +62,58 @@ router.post('/categories/new', (req, res) => {
                 res.redirect('/admin')
             })
     }
+})
+
+router.get('/category/edit/:id', (req, res) => {
+    Category.findOne({ _id: req.params.id })
+        .then((result) => {
+            res.render('admin/editcategory', { category: result })
+        })
+        .catch(() => {
+            req.flash('error_msg', 'Category doesnt exist')
+            res.redirect('/admin/categories')
+        })
+})
+
+router.post('/category/edit', (req, res) => {
+
+    Category.findOne({ _id: req.body.id })
+        .then((category) => {
+            var errors = []
+
+            if (!req.body.name || req.body.name == undefined || req.body.name == null) {
+                errors.push({ text: 'Invalid name' })
+            }
+
+            if (!req.body.slug || req.body.slug == undefined || req.body.slug == null) {
+                errors.push({ text: 'Invalid slug' })
+            }
+
+            if (req.body.name.length < 2) {
+                errors.push({ text: 'Category name too short' })
+            }
+
+            if (errors.length > 0) {
+                res.render('admin/editcategory', { errors: errors, category: category})
+            } else {
+
+                category.name = req.body.name
+                category.slug = req.body.slug
+                category.save()
+                    .then(() => {
+                        req.flash('success_msg', 'Category edited')
+                        res.redirect('/admin/categories')
+                    })
+                    .catch((err) => {
+                        req.flash('error_msg', 'Error saving category')
+                        res.redirect('/admin/category/edit/' + req.body.id)
+                    })
+            }
+        })
+        .catch(() => {
+            req.flash('error_msg', 'Error updating category')
+            res.redirect('/admin/category/edit/' + req.body.id)
+        })
 })
 
 module.exports = router
