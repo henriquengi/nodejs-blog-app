00650dd901336910d899eef162eab63235cd0eea
Henrique Souza <henrique.asouza@outlook.com>
2021-10-21 12:42:44 -0300

    chore: class-53

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    mail: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+mongoose.model('users', User)
